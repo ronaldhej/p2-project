7d1d0a6bc1f2fed50e8375e93f459c61d5a1ad29
Ronald J <ronaldhej@gmail.com>
2023-03-13 10:29:06 +0100

    Added rough physics and collision

--- entity.py
+++ entity.py
@@ -2,26 +2,24 @@ import math
 import arcade
 import pymunk
 
-PERSON_SPEED = 2
+PERSON_SPEED = 150
+
 
 class Person:
-   def __init__(self, center_x, center_y, radius, angle, delta_x, delta_y, delta_angle, color):
-      self.center_x = center_x
-      self.center_y = center_y
-      self.radius = radius
-      self.angle = angle
-      self.delta_x = delta_x
-      self.delta_y = delta_y
-      self.delta_angle = delta_angle
+   def __init__(self, pymunk_shape, color):
+      self.center_x = pymunk_shape.body.position.x
+      self.center_y = pymunk_shape.body.position.y
+      self.radius = pymunk_shape.radius
+      self.width = pymunk_shape.radius * 2
+      self.height = pymunk_shape.radius * 2
+      self.angle = pymunk_shape.body.angle
       self.color = color
+      self.pymunk_shape = pymunk_shape
       
    def draw(self):
-      arcade.draw_circle_filled(self.center_x, self.center_y, self.radius, self.color, self.angle)
+      arcade.draw_circle_filled(self.center_x, self.center_y, self.radius, self.color)
 
    def follow_mouse(self, mouse_x, mouse_y):
-      self.center_x += self.delta_x
-      self.center_y += self.delta_y
-
       start_x = self.center_x
       start_y = self.center_y
 
@@ -32,16 +30,16 @@ class Person:
       y_diff = dest_y - start_y
       angle = math.atan2(y_diff, x_diff)      
 
-      self.delta_x = math.cos(angle) * PERSON_SPEED
-      self.delta_y = math.sin(angle) * PERSON_SPEED
+      return math.cos(angle) * PERSON_SPEED, math.sin(angle) * PERSON_SPEED
 
 class Wall:
-   def __init__(self, center_x, center_y, width, height, color):
+   def __init__(self, pymunk_shape, center_x, center_y, width, height, color):
       self.center_x = center_x
       self.center_y = center_y
       self.width = width
       self.height = height
       self.color = color
+      self.pymunk_shape = pymunk_shape
 
    def set_vals(self, center_x, center_y, width, height):
       self.center_x = center_x
--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import math
 import logging
 import arcade
 import pymunk
@@ -15,34 +16,77 @@ class Simulator(arcade.Window):
     def __init__(self):
         super().__init__(SCREEN_WIDTH, SCREEN_HEIGHT, SCREEN_TITLE)
         arcade.set_background_color(arcade.color.BLACK)
+        self.space = pymunk.Space()
+        self.space.iterations = 35
+        self.space.gravity = (0.0, 0.0)
+        self.space.damping = 0.1
         self.person_list = []
         self.wall_list = []
+        self.static_lines = []
         self.selection_box = None
         self.paused = False
         self.mouse_x = 0
         self.mouse_y = 0
         self.box_start = (None, None)
         self.box_end = (None, None)
-        person = entity.Person(50, 50, 20, 10, 0, 0, 0, arcade.color.WHITE)
 
-        self.person_list.append(person)
+        #Add Map Boundaries
+        #Lower
+        body = pymunk.Body(body_type=pymunk.Body.STATIC)
+        shape = pymunk.Segment(body, (0, 0), (SCREEN_WIDTH, 0), 0.0)
+        shape.friction = 10
+        self.space.add(shape, body)
+        self.static_lines.append(shape)
+        #Upper
+        body = pymunk.Body(body_type=pymunk.Body.STATIC)
+        shape = pymunk.Segment(body, (0, SCREEN_HEIGHT), (SCREEN_WIDTH, SCREEN_HEIGHT), 0.0)
+        shape.friction = 10
+        self.space.add(shape, body)
+        self.static_lines.append(shape)
+        #Left
+        body = pymunk.Body(body_type=pymunk.Body.STATIC)
+        shape = pymunk.Segment(body, (0, 0), (0, SCREEN_HEIGHT), 0.0)
+        shape.friction = 10
+        self.space.add(shape, body)
+        self.static_lines.append(shape)
+        #Right
+        body = pymunk.Body(body_type=pymunk.Body.STATIC)
+        shape = pymunk.Segment(body, (SCREEN_WIDTH, 0), (SCREEN_WIDTH, SCREEN_HEIGHT), 0.0)
+        shape.friction = 10
+        self.space.add(shape, body)
+        self.static_lines.append(shape)
+        
 
     #Called on each update for the sake of drawing entities, sprites etc
     def on_draw(self):
         self.clear()
-        arcade.draw_text("Press space to pause/unpause entity movement, c to clear walls", 50, 50, arcade.color.WHITE, 12)        
+        arcade.draw_text("Press space to pause/unpause entity movement. Mouse left: wall, Mouse Right: Spawn Person", 50, 50, arcade.color.WHITE, 12)        
         for person in self.person_list:
             person.draw()
         for wall in self.wall_list:
             wall.draw()
+        #Draw Map Boundaries
+        for line in self.static_lines:
+            body = line.body
+            pv1 = body.position + line.a.rotated(body.angle)
+            pv2 = body.position + line.b.rotated(body.angle)
+            arcade.draw_line(pv1.x, pv1.y, pv2.x, pv2.y, arcade.color.WHITE, 5)
         if self.selection_box != None:
             self.selection_box.draw()
 
     #Tick Updates
     def on_update(self, dt):
-        if self.paused == False:
+        if self.paused:
+            return
+        else:
+            self.space.step(1/60)
             for person in self.person_list:
-                person.follow_mouse(self.mouse_x, self.mouse_y)
+                #Add force to follow mouse
+                person.pymunk_shape.body.apply_force_at_local_point(person.follow_mouse(self.mouse_x, self.mouse_y))
+                #Update draw position to physics object position
+                person.center_x = person.pymunk_shape.body.position.x
+                person.center_y = person.pymunk_shape.body.position.y
+                person.angle = math.degrees(person.pymunk_shape.body.angle)
 
     #Input Handling
     def on_mouse_motion(self, x, y, dx, dy):
@@ -54,31 +98,53 @@ class Simulator(arcade.Window):
             case arcade.key.SPACE:
                 self.paused = not self.paused
             case arcade.key.C:
+                for wall in self.wall_list:
+                    self.space.remove(wall.pymunk_shape, wall.pymunk_shape.body)                    
                 self.wall_list.clear()
             case _:
                 return
 
     def on_mouse_press(self, x, y, button, key_modifiers):
-        LOG.debug("Mouse pressed")
-        self.box_start = (x, y)
-        vals = get_rectangle_vals(x, y, x, y)
-        selection = entity.SelectionBox(*vals, arcade.color.WHITE, 2)
-        self.selection_box = selection
-
-    def on_mouse_drag(self, x: int, y: int, dx: int, dy: int, buttons: int, modifiers: int):
-        start_x, start_y = self.box_start
-        if start_x != None and start_y != None:
-            vals = get_rectangle_vals(start_x, start_y, x, y)
+        LOG.debug("Mouse pressed: %s", button)
+        if button == 1:
+            self.box_start = (x, y)
+            vals = get_rectangle_vals(x, y, x, y)
             selection = entity.SelectionBox(*vals, arcade.color.WHITE, 2)
             self.selection_box = selection
+        else:
+            inertia = pymunk.moment_for_circle(1, 0, 20, (0, 0))
+            body = pymunk.Body(1,  inertia)
+            body.position = x, y
+            shape = pymunk.Circle(body, 20, pymunk.Vec2d(0, 0))
+            shape.friction = 0.3
+            person = entity.Person(shape, arcade.color.WHITE)
+            self.space.add(body, shape)
+            self.person_list.append(person)
+
+    def on_mouse_drag(self, x: int, y: int, dx: int, dy: int, buttons: int, modifiers: int):
+        if buttons == 1:
+            start_x, start_y = self.box_start
+            if start_x != None and start_y != None:
+                vals = get_rectangle_vals(start_x, start_y, x, y)
+                selection = entity.SelectionBox(*vals, arcade.color.WHITE, 2)
+                self.selection_box = selection
+        else:
+            return
 
     def on_mouse_release(self, x, y, button, modifiers):
-        LOG.debug("Mouse just released")
-        self.selection_box = None
-        self.box_end = (x, y)
-        vals = get_rectangle_vals(*self.box_start, *self.box_end)
-        wall = entity.Wall(*vals, arcade.color.WHITE)
-        self.wall_list.append(wall)
+        if button == 1:
+            LOG.debug("Mouse just released")
+            self.selection_box = None
+            self.box_end = (x, y)
+            center_x, center_y, width, height = get_rectangle_vals(*self.box_start, *self.box_end)
+            body = pymunk.Body(body_type=pymunk.Body.STATIC)
+            body.position = pymunk.Vec2d(center_x, center_y)
+            shape = pymunk.Poly.create_box(body, (width, height))
+            wall = entity.Wall(shape, center_x, center_y, width, height, arcade.color.WHITE)
+            self.space.add(body, shape)
+            self.wall_list.append(wall)
+        else:
+            return
 
 #Utility Functions
 def get_rectangle_vals(start_x, start_y, end_x, end_y):
