e87a5d8306763295d72fe8a34c476f71813b672a
MathiasFM <mathiasmogensen1@gmail.com>
2023-05-13 00:59:52 +0200

    fixed uvicorn not running on setup

--- App.API/startup.sh
+++ App.API/startup.sh
@@ -4,7 +4,7 @@ if [ ! -e $CONTAINER_ALREADY_STARTED ]; then
     touch $CONTAINER_ALREADY_STARTED
     echo "-- First container startup --"
     echo "starting on display ${DISPLAY}..."
-    Xvfb "${DISPLAY}" -screen 0 1024x768x16
+    Xvfb "${DISPLAY}" -screen 0 1024x768x16 &
     sleep 1
 else
     echo "-- Not first container startup --"
